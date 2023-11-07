7db11b52df36da7526c6a3eb327cff76b508bdf0
khrystyna-shekel <christina.shekel@gmail.com>
2023-11-07 22:35:18 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryItemsList = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems.map(galleryItemInfo => {
+    const { preview, original, description } = galleryItemInfo;
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+    </a>
+    </li> `;
+});
+galleryItemsList.insertAdjacentHTML("beforeend", galleryItemsEl.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
 console.log(galleryItems);
